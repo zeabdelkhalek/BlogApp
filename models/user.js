55012e2a31a0eae36aee110f36d26fa582619adb
Zellat Abdelkhalek <34510420+AbdelkhalekESI@users.noreply.github.com>
2018-05-20 04:40:09 +0100

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose") ;
+var UserScheema = new mongoose.Schema ({
+    username : String ,
+    password : String
+})
+UserScheema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User" , UserScheema) ;
